c590f63e4afa92dd18512a0f090b251abbe86e93
Thomas Fricard <thomasfricard@gmail.com>
2021-07-02 15:53:44 +0200

    fix unfollow feature

--- litreview/flux/views.py
+++ litreview/flux/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.messages import messages
+from django.db import IntegrityError
 
 from .models import Ticket, Review
-from follows.models import UserFollows
+from ..follows.models import UserFollows
 
 def flux(request):
     current_user = request.user
@@ -86,6 +88,6 @@ def update(request):
 
         return redirect('flux')
 
-    return render(request, 'flux.html', context)
+    return render(request, 'flux.html')
 
 
--- litreview/follows/views.py
+++ litreview/follows/views.py
@@ -8,7 +8,6 @@ from .models import UserFollows
 
 def follows(request):
     if "follow" in request.POST: 
-        print("pourquoi c'est ici?")
         if request.method == 'POST':
             user_to_follow = request.POST.get('username', False)
             current_user = request.user
@@ -28,45 +27,23 @@ def follows(request):
                 messages.info(request, "L'utilisateur recherché n'existe pas.")
                 return redirect('follows')
 
-    user_followed = []
-    for follow in UserFollows.objects.filter(user=request.user):
-        user_followed.append(follow.followed_user.username)
+    user_followed = UserFollows.objects.filter(user=request.user).order_by('followed_user__username')
 
+    for follow in user_followed:
+        print(follow.followed_user)
+   
     context = {'follows': user_followed}
 
     return render(request, 'follows.html', context=context)
 
-"""def unfollows(request):
-    if request.method == 'POST':
-        user_to_unfollow = request.POST['username']
-        current_user = request.user
-
-        if User.objects.filter(username=user_to_unfollow).exists():
-            unfollowed_user = User.objects.get(username=user_to_unfollow)
-            
-            try:
-                unfollow = UserFollows.objects.delete(user=current_user, followed_user=unfollowed_user)
-                unfollow.save()
-            except IntegrityError:
-                messages.info(request, "Vous ne pouvez supprimer un utilisateur qui n'est pas dans votre liste.")
-                return redirect('follows')
-        else:
-            messages.info(request, "L'utilisateur recherché n'existe pas.")
-            return redirect('follows')
-
-    context = {'unfollows': user_to_unfollow}
-
-    return render(request, 'unfollows.html', context)
-"""
 
 def unfollows(request):
-    if 'unfollow' in request.POST:
-        user_to_unfollow = request.POST.get("unfollow")
-        print(f"User unfollowed: {user_to_unfollow}")
+    if 'unfollow_button' in request.POST:
+        user_to_unfollow = request.POST.get('unfollow_button')
         unfollowed_user = UserFollows.objects.filter(followed_user__username=user_to_unfollow).delete()
 
         messages.info(request, f"L'utilisateur {user_to_unfollow} a bien été unfollow.")
-        
+            
         return redirect('follows')
 
 def print_dict(context):
